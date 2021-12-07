b56b3d41a48856c7bc227e5b5bc734f511615e13
Michael <mlmbufpi@gmail.com>
2021-12-07 16:53:38 -0300

    feat: implementa interface
    feat: conecta com api

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -2,8 +2,10 @@ import { domInjector } from '../decorators/dom-injector.js';
 import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -17,6 +19,8 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+
+    private negociacaoService = new NegociacoesService()
 
     constructor() {
         // this.inputData = <HTMLInputElement>document.querySelector('#data');
@@ -46,6 +50,18 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    public importarDados(): void{
+        this.negociacaoService
+        .obterNegociacoes()
+        .then(negociacoesDeHoje => {
+            for(let negociacao of negociacoesDeHoje){
+                this.negociacoes.adiciona(negociacao)
+            }
+            this.negociacoesView.update(this.negociacoes)
+        })
+
     }
 
     private ehDiaUtil(data: Date) {
