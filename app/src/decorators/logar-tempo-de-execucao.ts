582126fdb6d5ca946ac1d610b2aeee283ca8ca19
Michael <mlmbufpi@gmail.com>
2021-12-07 08:46:21 -0300

    feat: implementa o decorator para medir tempo de exec

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,14 +1,17 @@
 export function logarTempoDeExecucao() {
     return function(
         target: any,
-        propertyKey: string,
+        propertyKey: string, //nome do método
         descriptor: PropertyDescriptor
     ) {
         const metodoOriginal = descriptor.value;
-        descriptor.value = function(...args: any[]) {
+        descriptor.value = function(...args: any[]) {//obtendo os parametros da funcao englobada pelo Decorator
             const t1 = performance.now();
-            const retorno = metodoOriginal.apply(this, args);
+           
+            const retorno = metodoOriginal.apply(this, args); //obtendo o retorno e realocando o this para 
+            //ser igual ao contexto da funcao que é chamada.
             const t2 = performance.now();
+           
             console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`);
             retorno
         };
