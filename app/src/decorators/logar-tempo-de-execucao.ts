a59e9894cb8b5a10486244b68e5184951f57ccdd
Michael <mlmbufpi@gmail.com>
2021-12-07 15:10:38 -0300

    feat: cria mais decorators: inspect

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(
         target: any,
         propertyKey: string, //nome do método
@@ -6,13 +6,20 @@ export function logarTempoDeExecucao() {
     ) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: any[]) {//obtendo os parametros da funcao englobada pelo Decorator
+            
+            let divisor = 1; 
+            let unidade = 'milisegundos'
+            if(emSegundos){
+                divisor = 1000;
+                unidade = 'segundos'
+            }
             const t1 = performance.now();
            
             const retorno = metodoOriginal.apply(this, args); //obtendo o retorno e realocando o this para 
             //ser igual ao contexto da funcao que é chamada.
             const t2 = performance.now();
            
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/divisor} ${unidade}`);
             retorno
         };
 
